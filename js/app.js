c8de9b62d7f245cb69e606523e6794e0c12b1d42
Maycol234 <kevinlopezacevedo905@gmail.com>
2026-04-17 12:33:02 -0600

    Update project links to GitHub Pages

--- js/app.js
+++ js/app.js
@@ -1,39 +1,39 @@
 
 const proyectos = [
     {
-        nombre: "APP Programación",
-        descripcion: "Aplicación de programación - Primer parcial",
-        enlace: "https://github.com/Maycol234/APP_PROGRAMACION",
-        icono: "💻"
-    },
-    {
         nombre: "Streaming HTML5",
         descripcion: "Plataforma de streaming desarrollada en HTML5",
-        enlace: "https://github.com/Maycol234/Streaming_HTML5",
+        enlace: "https://maycol234.github.io/Streaming_HTML5/",
         icono: "🎬"
+    },
+    {
+        nombre: "APP Programación",
+        descripcion: "Aplicación de programación - Primer parcial",
+        enlace: "https://maycol234.github.io/APP_PROGRAMACION/",
+        icono: "💻"
     },
     {
         nombre: "P3 iOS Ejemplo App",
         descripcion: "Aplicación ejemplo para iOS",
-        enlace: "https://github.com/Maycol234/P3_IOS_EJEMPLOAPP",
+        enlace: "https://maycol234.github.io/P3_IOS_EJEMPLOAPP/",
         icono: "🍎"
     },
     {
         nombre: "APP iOS",
         descripcion: "Aplicación iOS desarrollada",
-        enlace: "https://github.com/Maycol234/APP_IOS",
+        enlace: "https://maycol234.github.io/APP_IOS/",
         icono: "📲"
     },
     {
         nombre: "Sitio Web HTML5",
         descripcion: "Sitio web profesional en HTML5",
-        enlace: "https://github.com/Maycol234/SITIO_WEB_HTML5",
+        enlace: "https://maycol234.github.io/SITIO_WEB_HTML5/",
         icono: "🌐"
     },
     {
         nombre: "Práctica 2 - Sub2",
         descripcion: "Práctica 2 - Subtema 2 del primer parcial",
-        enlace: "https://github.com/Maycol234/practica2-sub2-KevinMichell",
+        enlace: "https://maycol234.github.io/practica2-sub2-KevinMichell/",
         icono: "📝"
     }
 ];
@@ -49,7 +49,7 @@ function cargarProyectos() {
         card.innerHTML = `
             <h3>${proyecto.icono} ${proyecto.nombre}</h3>
             <p>${proyecto.descripcion}</p>
-            <a href="${proyecto.enlace}" target="_blank" class="btn">Ver en GitHub</a>
+            <a href="${proyecto.enlace}" target="_blank" class="btn">Ver Proyecto</a>
         `;
         
         contenedor.appendChild(card);
